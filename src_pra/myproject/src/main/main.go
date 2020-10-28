634013587d31f0064d2fbda229444203d6ee120a
DenTaku <dentaku1412@gmail.com>
2020-10-28 22:09:14 +0900

    Learn loop

--- src_pra/myproject/src/main/main.go
+++ src_pra/myproject/src/main/main.go
@@ -4,6 +4,52 @@ import (
 	."strings"
 )
 
+// var message string = "hello world"
+
+var foo, bar, buz string = "foo", "bar", "buz"
+
+var (
+	a string = "aaa"
+	b = "bbb"
+	c = "ccc"
+	d = "ddd"
+	e = "eee"
+)
+
 func main() {
-	f.Println(ToUpper("hello world")) // hello world
+	message := "hello world"
+	const Hello string = "hello"
+	// Hello = "bye" error
+	var i int
+	f.Println(i)
+	f.Println(ToUpper(message))
+
+	a, b := 10, 100
+	if a > b {
+		f.Println("a is larger than b")
+	} else if a < b {
+		f.Println("a is smaller than b")
+	} else {
+		f.Println("a equals b")
+	} 
+
+	for i := 0; i < 10; i++ {
+		f.Println(i)
+	}
+	n := 0
+	for n < 10 {
+		f.Printf("n = %d\n", n)
+		n++
+	}
+	m := 0
+	for {
+		m++
+		if m > 10 {
+			break
+		}
+		if m % 2 == 0 {
+			continue
+		}
+		f.Println(m)
+	}
 }
